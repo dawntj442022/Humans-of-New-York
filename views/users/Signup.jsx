f3308d8a36b01eb9059c8625f628c09de38b5a8e
dawntj44 <dawntj44@gmail.com>
2023-02-03 23:56:43 -0500

    default layout not working in virw and user folders

--- views/users/Signup.jsx
+++ views/users/Signup.jsx
@@ -3,48 +3,16 @@ const React = require("react");
 class Signup extends React.Component {
   render() {
     return (
-      <DefaultLayout>
-        <div>
-          <h1 className="font-extrabold text-blue-500 text-center text-3xl underline big-clip-text">
-            Humans of New York
-          </h1>
-          <h3 className="font-extrabold text-blue-500 text-center text-xl">
-            Signup
-          </h3>
+      <div>
+        <h1>Signup</h1>
 
-          <form action="/user/signup" method="POST">
-            Name: <input type="text" defaultValue="" name="username" required />
-            Password:{" "}
-            <input type="password" defaultValue="" name="password" required />
-            <input type="submit" name="" value="Signup" />
-          </form>
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <footer class="bg-gray-900 p-6 text-white">
-          <p>Copyright © 2023. All rights reserved.</p>
-        </footer>
-      </DefaultLayout>
+        <form action="/user/signup" method="POST">
+          Name: <input type="text" defaultValue="" name="username" required />
+          Password:{" "}
+          <input type="password" defaultValue="" name="password" required />
+          <input type="submit" name="" value="Signup" />
+        </form>
+      </div>
     );
   }
 }
